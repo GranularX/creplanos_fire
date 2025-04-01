9d771f641498d9e69717151d66236720ced59069
CreplanosHQ <we.creplanos@gmail.com>
2025-04-01 02:02:31 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export function Navbar() {
             <>
               {/* Desktop Navigation */}
               <div className="hidden md:flex items-center">
-               <NavLink to="https://taskboard.creplanos.com" icon={User}>Task Dashboard</NavLink>
+               <NavLink to="https://taskboard.creplanos.com" icon={User}>Taskboard</NavLink>
 
 {/*                 <NavLink to="/user-profile" icon={User}>User</NavLink> */}
                 <NavLink to="/wallet" icon={Wallet}>Wallet</NavLink>
@@ -68,11 +68,11 @@ export function Navbar() {
         {/* Mobile Navigation */}
         {!isAuthPage && isMenuOpen && (
           <div className="md:hidden py-4 space-y-2 border-t border-white/10">
-            <NavLink to="/provider-profile" icon={User}>Provider</NavLink>
+     <NavLink to="https://taskboard.creplanos.com" icon={User}>Taskboard</NavLink>
             <NavLink to="/user-profile" icon={User}>User</NavLink>
             <NavLink to="/wallet" icon={Wallet}>Wallet</NavLink>
             <NavLink to="/api" icon={Code}>API</NavLink> {/* Updated Icon for API */}
-              <NavLink to="/learn" icon={Book}>Learn</NavLink> {/* Updated Icon for API */}
+               <NavLink to="https://learnsy.creplanos.com" icon={Book}>LearnsyAI</NavLink> {/* Updated Icon for API */}
             <div className="pt-2 flex flex-col gap-2">
               <Link
                 to="/signin"
